0ca0597b222e71bb6562666fd10b1b74f45a1731
MR-T0NY <antonio.bandac@uanl.edu.mx>
2023-11-11 00:40:27 -0600

    Practica 12 actualizado

--- src/app/foto-service.service.ts
+++ src/app/foto-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
-import { Foto } from './foto.mode';
+import { Foto } from './foto.model';
 
 @Injectable({
   providedIn: 'root'
